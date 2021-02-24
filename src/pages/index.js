a27f46f90f5ad23222099d7abc99c4606ef2fccf
noahweinflash <79377261+noahweinflash@users.noreply.github.com>
2021-02-23 21:45:52 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,10 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Ready to get Loc\'tin? </p>
+    <p>Ready to get Loc'tin? </p>
     <p>View the prediction categories below.</p>
+  <form action="https://www.w3docs.com/">
+         <button type="submit">Political predictions</button>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
