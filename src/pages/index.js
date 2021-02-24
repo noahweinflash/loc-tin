aa4c5010f64362162286fbed2eb02792121fba7c
noahweinflash <79377261+noahweinflash@users.noreply.github.com>
2021-02-23 21:32:42 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Ready to get Loc\'tin? </p>
+    <p>View the prediction categories below.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
